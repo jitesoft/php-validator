d75e8b3719af2c8b47400959a57972a3ba867ed6
Johannes <johannes@jitesoft.com>
2018-03-26 15:51:25 +0200

    Implemented test for number rule.

--- src/Rules/Number.php
+++ src/Rules/Number.php
@@ -11,6 +11,33 @@ namespace Jitesoft\Validator\Rules;
  * @author Johannes Tegnér <johannes@jitesoft.com>
  * @version 1.0.0
  */
-class Number {
+class Number extends AbstractRule {
+    public const NAME        = 'number';
+    public const DESCRIPTION = 'Tests a value so that it is a number.';
 
+    /**
+     * @internal
+     */
+    public function __construct() {
+        $this->rules[] = Minimum::class;
+        $this->rules[] = Maximum::class;
+    }
+
+
+    /**
+     * Test the rule.
+     *
+     * @param $value
+     * @param array $rules
+     * @param array $args
+     * @return bool
+     */
+    protected function testRule($value, array $rules = [], $args = []): bool {
+        if (!is_numeric($value)) {
+            $this->error = 'Value was not a number.';
+            return false;
+        }
+
+        return true && $this->testSubRules($value, $rules);
+    }
 }
--- tests/Rules/NumberTest.php
+++ tests/Rules/NumberTest.php
@@ -6,11 +6,81 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 namespace Jitesoft\Validator\Tests\Rules;
 
+use Jitesoft\Validator\Contracts\RuleInterface;
+use Jitesoft\Validator\Rules\Factory;
+use Jitesoft\Validator\Rules\Number;
+use Jitesoft\Validator\Tests\AbstractTestCase;
+
 /**
  * NumberTest
  * @author Johannes Tegnér <johannes@jitesoft.com>
  * @version 1.0.0
  */
-class NumberTest {
+class NumberTest extends AbstractTestCase {
+
+    /** @var RuleInterface */
+    protected $rule;
+
+    protected function setUp() {
+        parent::setUp();
+
+        $this->rule = (new Factory())->create(Number::class);
+    }
+
+    public function testErrors() {
+        $this->rule->test('nan', ['number']);
+        $this->assertEquals(['number' => 'Value was not a number.'], $this->rule->getErrors());
+        $this->rule->test(123, ['number' => [ 'min' => 124 ]]);
+        $this->assertEquals(['min' => 'Value was lower than minimum bounds.'], $this->rule->getErrors());
+        $this->rule->test(300, ['number' => [ 'max' => 100 ]]);
+        $this->assertEquals(['max' => 'Value was higher than maximum bounds.'], $this->rule->getErrors());
+    }
+
+    public function testTest() {
+        $this->assertTrue($this->rule->test(123, [
+            'number' => [
+                'max' => 124,
+                'min' => 122
+            ]
+        ]));
+
+        $this->assertTrue($this->rule->test(122.6, [
+            'number' => [
+                'max' => 124.5,
+                'min' => 122.5
+            ]
+        ]));
+
+        $this->assertTrue($this->rule->test("122.6", [
+            'number' => [
+                'max' => 124.5,
+                'min' => 122.5
+            ]
+        ]));
+
+        $this->assertTrue($this->rule->test("122", [ 'number' ]));
+        $this->assertTrue($this->rule->test(0x123, [ 'number' ]));
+        $this->assertTrue($this->rule->test(1e5, [ 'number' ]));
+        $this->assertTrue($this->rule->test(123, [ 'number' ]));
+
+        $this->assertFalse($this->rule->test(122.6, [
+            'number' => [
+                'max' => 121.5,
+                'min' => 1
+            ]
+        ]));
+
+        $this->assertFalse($this->rule->test(122.6, [
+            'number' => [
+                'min' => 123
+            ]
+        ]));
+
+        $this->assertFalse($this->rule->test("hi", [ 'number' ]));
+        $this->assertFalse($this->rule->test("abc", [ 'number' ]));
+        $this->assertFalse($this->rule->test(true, [ 'number' ]));
+        $this->assertFalse($this->rule->test(false, [ 'number' ]));
+    }
+
 
 }
