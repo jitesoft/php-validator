391b48e08edf8c74558a3dd0b862579303f1c486
Johannes <johannes@jitesoft.com>
2018-03-26 16:25:50 +0200

    Fixed float test.

--- tests/Rules/FloatNumberTest.php
+++ tests/Rules/FloatNumberTest.php
@@ -6,11 +6,41 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 namespace Jitesoft\Validator\Tests\Rules;
 
+use Jitesoft\Validator\Contracts\RuleInterface;
+use Jitesoft\Validator\Rules\Factory;
+use Jitesoft\Validator\Rules\FloatNumber;
+use Jitesoft\Validator\Tests\AbstractTestCase;
+
 /**
  * FloatNumberTest
  * @author Johannes Tegnér <johannes@jitesoft.com>
  * @version 1.0.0
  */
-class FloatNumberTest {
+class FloatNumberTest extends AbstractTestCase {
+
+    /** @var RuleInterface */
+    protected $rule;
+
+    protected function setUp() {
+        parent::setUp();
+        $this->rule = (new Factory())->create(FloatNumber::class);
+    }
+
+    public function testErrors() {
+        $this->rule->test('adsdas', [ 'float' ]);
+        $this->assertEquals(['float' => 'Value was no a float.'], $this->rule->getErrors());
+    }
+
+    public function testTest() {
+        $this->assertTrue($this->rule->test(5.1, ['float']));
+        $this->assertTrue($this->rule->test(4.2, ['float' => ['min' => 3]]));
+        $this->assertTrue($this->rule->test(7.9, ['float' => ['max' => 8]]));
+
+
+        $this->assertFalse($this->rule->test(7, ['float']));
+        $this->assertFalse($this->rule->test("7.9", ['float']));
+        $this->assertFalse($this->rule->test(false, ['float']));
+        $this->assertFalse($this->rule->test(7.1, ['float' => ['min' => 8]]));
+    }
 
 }
