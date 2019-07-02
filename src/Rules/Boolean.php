762eceffbf7194c8440b10220e1ba839438bfacc
Johannes Tegnér <johannes@jitesoft.com>
2019-07-02 15:04:20 +0200

    Added 'boolean' rule.

---
+++ src/Rules/Boolean.php
@@ -0,0 +1,34 @@
+<?php
+// phpcs:ignoreFile
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  Boolean.php - Part of the validator project.
+
+  © - Jitesoft 2019
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Jitesoft\Validator\Rules;
+
+
+class Boolean extends AbstractRule {
+    public const NAME        = 'boolean';
+    public const DESCRIPTION = 'Tests a value to see if its a boolean value or not.';
+
+    /**
+     * Test the rule.
+     *
+     * @param mixed $value Value to validate.
+     * @param array $rules Rules to apply.
+     * @param array $args  Arguments.
+     * @return bool
+     * @since 1.0.0
+     */
+    protected function testRule($value, array $rules = [], array $args = []): bool {
+        $result = is_bool($value);
+        if (!$result) {
+            $this->error = 'Value was not a boolean value.';
+            $result = false;
+        }
+
+        return $this->testSubRules($value, $rules) && $result;
+    }
+
+}
---
+++ tests/Rules/BooleanTest.php
@@ -0,0 +1,43 @@
+<?php
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+  BooleanTest.php - Part of the validator project.
+
+  © - Jitesoft 2019
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+namespace Jitesoft\Validator\Tests\Rules;
+
+use Jitesoft\Validator\Contracts\RuleInterface;
+use Jitesoft\Validator\Rules\Boolean;
+use Jitesoft\Validator\Rules\Factory;
+use Jitesoft\Validator\Tests\AbstractTestCase;
+
+/**
+ * IntegerTest
+ * @author Johannes Tegnér <johannes@jitesoft.com>
+ * @version 1.0.0
+ */
+class BooleanTest extends AbstractTestCase {
+
+    /** @var RuleInterface */
+    protected $rule;
+
+    protected function setUp() {
+        parent::setUp();
+
+        $this->rule = (new Factory())->create(Boolean::class);
+    }
+
+    public function testErrors() {
+        $this->rule->test('abc', [ 'boolean' ]);
+        $this->assertEquals(['boolean' => 'Value was not a boolean value.'], $this->rule->popErrors());
+    }
+
+    public function testTest() {
+        $this->assertTrue($this->rule->test(true, [ 'boolean' ]));
+        $this->assertTrue($this->rule->test(false, [ 'boolean' ]));
+        $this->assertFalse($this->rule->test(new class {}, [ 'boolean' ]));
+        $this->assertFalse($this->rule->test(null, [ 'boolean' ]));
+        $this->assertFalse($this->rule->test("123", [ 'boolean' ]));
+    }
+
+}
